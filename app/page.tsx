f8f690991762c8c3b0ca9dda0a500fb459ae4349
Reynaldo Moreno <reynaldomoreno173@gmail.com>
2024-10-26 05:08:18 -0400

    creando slidervideo del home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { NavBar } from '@/components/Shared/NavBar';
+import { SliderVideo } from './(routes)/(home)/components/SliverVideo/SliderVideo';
 
 export default function Home() {
 	return (
 		<div className="relative bg-zinc-900">
 			<NavBar />
+			<SliderVideo />
 		</div>
 	);
 }
